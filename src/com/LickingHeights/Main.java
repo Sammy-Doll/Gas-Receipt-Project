fdd404e212995afe3e7c8b57954727df50e252a9
Sammy Doll <samuel.doll.2023@student.lhschools.org>
2020-02-24 14:01:24 -0500

    Intellij Commit

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,25 @@
+package com.LickingHeights;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("+-------------------------+");
+        System.out.println("| Thjnp's Gas Sation      |");
+        System.out.println("| 5782 Zargoth Drive      |");
+        System.out.println("| 6969 Bloody Creek Lane  |");
+        System.out.println("| Date          Time      |");
+        System.out.println("| 16JAN2020     4:20 PM   |");
+        System.out.println("| Invoice Number# 614946  |");
+        System.out.println("| Auth#                   |");
+        System.out.println("| Mastercard              |");
+        System.out.println("|*************78          |");
+        System.out.println("| pump    Product   $/G   |");
+        System.out.println("| 01      unleaded  2.54  |");
+        System.out.println("| Amount     Fuel Total   |");
+        System.out.println("| 20.00      $50.80       |");
+        System.out.println("|                         |");
+        System.out.println("|Thanks For Buying From Us|");
+        System.out.println("|   Thjnp's Gas Station   |");
+        System.out.println("+-------------------------+");
+    }
+}
